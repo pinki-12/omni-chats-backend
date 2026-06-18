497b8b2333bfeaf2c55fa938ed2d203dd20f7e1c
pinki-12 <pk0540080@gmail.com>
2026-06-18 11:33:20 +0530

    third commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 //  middlewares:
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://text07.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://text07.vercel.app","https://omni-chats-frontend-z1ir.vercel.app"],
     credentials: true,
   }),
 );
