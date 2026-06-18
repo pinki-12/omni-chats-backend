e78da7cbdd12cd499a5bb2afc9a965dadbb52156
pinki-12 <pk0540080@gmail.com>
2026-06-18 14:01:45 +0530

    changes in middleware

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -1,32 +1,6 @@
-// import jwt from "jsonwebtoken";
-// export const protect = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.token;
-
-//     if (!token) {
-//       return res.status(401).json({
-//         message: "UnAuthorized",
-//       });
-//     }
-//     const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-//     if (!decoded) {
-//       return res.status(404).json({
-//         message: "user not found",
-//       });
-//     }
-//     req.user = decoded;
-//     next();
-//   } catch (err) {
-//     return res.status(500).json({
-//       message: err.message,
-//     });
-//   }
-// };
-
+import jwt from "jsonwebtoken";
 export const protect = async (req, res, next) => {
   try {
-    console.log("Cookies:", req.cookies);
-
     const token = req.cookies.token;
 
     if (!token) {
@@ -34,18 +8,18 @@ export const protect = async (req, res, next) => {
         message: "UnAuthorized",
       });
     }
-
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    );
-
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    }
     req.user = decoded;
     next();
   } catch (err) {
-    console.log(err);
     return res.status(500).json({
       message: err.message,
     });
   }
 };
+
